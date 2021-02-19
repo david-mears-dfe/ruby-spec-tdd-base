b2f2998f982641c5c297ff0991cbe27d09a70836
David Mears <david.mears@digital.education.gov.uk>
2021-02-19 18:08:23 +0000

    Get rid of example specs and methods, and guard-rspec gem

--- Gemfile
+++ Gemfile
@@ -2,6 +2,5 @@ source 'https://rubygems.org'
 
 group :development do
   gem 'rspec'
-  gem 'guard-rspec', require: false
   gem 'pry'
 end
--- lib/example.rb
+++ lib/example.rb
@@ -1,11 +1,3 @@
 class Example
-  attr_accessor :a, :b
-
-  def initialize(a: 0, b: 0)
-    @a, @b = a, b
-  end
-
-  def add
-    a + b
-  end
+  def initialize; end
 end
